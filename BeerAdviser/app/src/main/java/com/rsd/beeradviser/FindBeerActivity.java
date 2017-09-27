3ca60db9c3b151d435733771c93751f7fc9c6ff9
Viacheslav Romanov <romanov.vyu@gmail.com>
2017-09-27 17:41:20 +0300

    add code to FindBeerActivity

--- BeerAdviser/app/src/main/java/com/rsd/beeradviser/FindBeerActivity.java
+++ BeerAdviser/app/src/main/java/com/rsd/beeradviser/FindBeerActivity.java
@@ -2,6 +2,9 @@ package com.rsd.beeradviser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class FindBeerActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class FindBeerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+
+    }
+
+    //Вызывается при щелчке на кнопке
+    public void onClickFindBeer(View view) {
+        //Получить ссылку на TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+        //Получить ссылку на Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+        //Получить вариант, выбранный в Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+        //Вывести выбранный вариант
+        brands.setText(beerType);
     }
 }
