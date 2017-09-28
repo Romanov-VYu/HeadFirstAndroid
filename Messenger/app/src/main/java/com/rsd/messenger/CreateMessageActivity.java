9fd9ad39dea2ffabd82069fac92d6aed20321a1e
Viacheslav Romanov <romanov.vyu@gmail.com>
2017-09-28 14:44:47 +0300

    add parameters to intent

--- Messenger/app/src/main/java/com/rsd/messenger/CreateMessageActivity.java
+++ Messenger/app/src/main/java/com/rsd/messenger/CreateMessageActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends AppCompatActivity {
 
@@ -15,9 +16,13 @@ public class CreateMessageActivity extends AppCompatActivity {
 
     //Вызвать onSendMessage() при щелчке на кнопке
     public void onSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
         // Создание Intent'а
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
         // Открытие второй активности.
         startActivity(intent);
+
     }
 }
