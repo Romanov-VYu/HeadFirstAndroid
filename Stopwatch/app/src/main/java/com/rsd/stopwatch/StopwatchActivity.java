412d3f124212e308b29364af00a71774f916d013
Viacheslav Romanov <romanov.vyu@gmail.com>
2017-10-04 14:10:03 +0300

    Change method onStart to onResume and onStop to onPause

--- Stopwatch/app/src/main/java/com/rsd/stopwatch/StopwatchActivity.java
+++ Stopwatch/app/src/main/java/com/rsd/stopwatch/StopwatchActivity.java
@@ -62,14 +62,14 @@ public class StopwatchActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onPause() {
+        super.onPause();
         wasRunning = running;
         running = false;
     }
     @Override
-    protected void onStart() {
-        super.onStart();
+    protected void onResume() {
+        super.onResume();
         if (wasRunning) {
             running = true;
         }
